df89648639bf681e02819534852f9de35158b263
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 19:59:34 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan2-DeklarasiVariabel/src/IF0110118039Latihan2DeklarasiVariabel.java
@@ -0,0 +1,34 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta Uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        //Memberi Nilai ke Variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+
+}
